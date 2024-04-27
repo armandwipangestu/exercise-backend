9280f2e486ce4ed1a8eaaa6dcae8a24535ef5180
Arman <armandwi.pangestu7@gmail.com>
2024-04-27 12:30:52 +0700

    feat(app): add cookie parser

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+
 import postController from "./post/post.controller.js";
 import userController from "./user/user.controller.js";
 import categoryController from "./category/category.controller.js";
@@ -11,10 +13,13 @@ dotenv.config();
 
 app.use(
     cors({
+        credentials: true,
         origin: "*",
     })
 );
+app.use(cookieParser());
 app.use(express.json());
+
 app.use(`${process.env.API_VERSION}`, postController);
 app.use(`${process.env.API_VERSION}`, userController);
 app.use(`${process.env.API_VERSION}`, categoryController);
