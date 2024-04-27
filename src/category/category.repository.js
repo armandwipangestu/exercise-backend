04f154850351270ff35d2ac9acf5fbc13bdeabb8
Arman <armandwi.pangestu7@gmail.com>
2024-04-28 02:06:35 +0700

    feat(query): include posts data when query categories

--- src/category/category.repository.js
+++ src/category/category.repository.js
@@ -1,7 +1,11 @@
 import prisma from "../../db/prisma.js";
 
 const findCategories = async () => {
-    const categories = await prisma.category.findMany({});
+    const categories = await prisma.category.findMany({
+        include: {
+            posts: true,
+        },
+    });
 
     return categories;
 };
